a0a487c28676954907916f9781d714963975dedb
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-02-14 19:00:29 +0300

    add page as request param

--- src/main/kotlin/com/example/kotlin_rest/Controller/CountryController.kt
+++ src/main/kotlin/com/example/kotlin_rest/Controller/CountryController.kt
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,7 +17,9 @@ class CountryController (
 
 ){
     @GetMapping
-    fun getAll(): List<CountyDto> = countryService.getAll()
+    fun getAll(@RequestParam("page") pageIndex: Int): List<CountyDto> =
+
+        countryService.getAll(pageIndex)
 
 
 }
