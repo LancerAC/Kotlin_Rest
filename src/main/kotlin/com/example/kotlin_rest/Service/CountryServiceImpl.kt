c1a970e2b4620bb8dfb6f4567deba576b4eed244
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-02-14 18:09:16 +0300

    add countryRepository for ger all countries

--- src/main/kotlin/com/example/kotlin_rest/Service/CountryServiceImpl.kt
+++ src/main/kotlin/com/example/kotlin_rest/Service/CountryServiceImpl.kt
@@ -1,16 +1,26 @@
 package com.example.kotlin_rest.Service
 
 import com.example.kotlin_rest.Dto.CountyDto
+import com.example.kotlin_rest.Entity.CountryEntity
+import com.example.kotlin_rest.Repository.CountryRepository
 import org.springframework.stereotype.Service
 
 @Service
-class CountryServiceImpl : CountryService  {
+class CountryServiceImpl(
+    private val countryRepository: CountryRepository
+) : CountryService
+{
     override fun getAll(): List<CountyDto> {
-        return listOf(
-            CountyDto(1, "USA",
-                345265),
-            CountyDto(2, "Russia",
-                3523523)
-        )
+        return countryRepository.findAll().map {
+            it.toDto()
+        }
     }
-}
+
+    private fun CountryEntity.toDto(): CountyDto =
+        CountyDto(
+            id = this.id,
+            name = this.name,
+            population = this.population,
+        )
+
+    }
