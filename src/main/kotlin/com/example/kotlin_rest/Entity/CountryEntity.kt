7a6e8da83c15c0c247f24a1de362b545c0f5ae07
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-02-17 15:48:04 +0300

    add builder for CountryEntity

--- src/main/kotlin/com/example/kotlin_rest/Entity/CountryEntity.kt
+++ src/main/kotlin/com/example/kotlin_rest/Entity/CountryEntity.kt
@@ -6,17 +6,27 @@ import lombok.Builder
 import lombok.Data
 import lombok.NoArgsConstructor
 
-@Entity
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "country")
-class CountryEntity (
 
+@Entity
+@Table(name = "country")
+data class CountryEntity(
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int = 0,
     val name: String = "",
     val population: Int = 0
-)
+) {
+    data class Builder(
+        var id: Int = 0,
+        var name: String = "",
+        var population: Int = 0
+    ) {
+        fun id(id: Int) = apply { this.id = id }
+        fun name(name: String) = apply { this.name = name }
+        fun population(population: Int) = apply { this.population = population }
+
+        fun build() = CountryEntity(id, name, population)
+    }
+}
+
+
