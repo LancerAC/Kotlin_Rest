7f32ad2d86b752ec4b335815c0930fcb8d69f55b
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-02-17 19:24:20 +0300

    add list of cities

--- src/main/kotlin/com/example/kotlin_rest/Dto/CountyDto.kt
+++ src/main/kotlin/com/example/kotlin_rest/Dto/CountyDto.kt
@@ -1,7 +1,12 @@
+package com.example.kotlin_rest.Dto
+
+import com.example.kotlin_rest.Entity.CityEntity
+
 data class CountryDto(
     val id: Int,
     val name: String,
-    val population: Int
+    val population: Int,
+    val cityList: List<CityDto>
 )
 
 
