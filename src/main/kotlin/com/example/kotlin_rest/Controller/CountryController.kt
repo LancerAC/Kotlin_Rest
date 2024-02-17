b54c878327e12a21ca7aaaf1ff2a0e5164d88d6b
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-02-17 14:50:13 +0300

    realize new method for get country by id in controller

--- src/main/kotlin/com/example/kotlin_rest/Controller/CountryController.kt
+++ src/main/kotlin/com/example/kotlin_rest/Controller/CountryController.kt
@@ -5,6 +5,7 @@ import com.example.kotlin_rest.Service.CountryService
 import lombok.RequiredArgsConstructor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -21,5 +22,8 @@ class CountryController (
 
         countryService.getAll(pageIndex)
 
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Int): CountyDto =
+        countryService.findById(id)
 
 }
